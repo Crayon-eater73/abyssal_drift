67f29002b8c0021ab88730284c4ce350c19fd8e1
Crayon-eater73 <4hartrm@gmail.com>
2026-05-14 16:31:45 -0700

    Update README and started writing the JFrame

---
+++ src/panel.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class panel extends JPanel {
+
+    final int originalTileSize = 16;
+    final int scale = 3;
+    
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenLong = 16;
+    final int maxScreenLan = 12;
+
+    final int screenWidth = tileSize * maxScreenLong;
+    final int screenHeight = tileSize * maxScreenLan;
+
+    public panel() {
+        setPreferredSize(new Dimension(screenWidth, screenHeight));
+        setBackground(Color.black);
+        setDoubleBuffered(true);
+    }
+}
